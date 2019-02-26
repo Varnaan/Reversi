6bf021c89f8469f0669e068ebdc10be0d5fe7496
Varnaan <derpbort@gmail.com>
2019-02-26 15:28:14 +0100

    index added

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
-const static = require('node-static');
-const http = require('http');
+let static = require('node-static');
+let http = require('http');
 
 let port = process.env.PORT;
 let directory = __dirname + '/public';
 
-if(typeof port == 'undefined' || !port){
+if (typeof port == 'undefined' || !port) {
     directory = './public';
     port = 8080;
 }
 
-let file = new.static.Server(directory);
+let file = new static.Server(directory);
 
 const app = http.createServer(
-    function(req, res) {
-        request.addListener('end' , 
-            function() {
-                file.server(req,res);
+    function (req, res) {
+        request.addListener('end',
+            function () {
+                file.server(req, res);
             }
         ).resume();
     }
 ).listen(port);
+
+console.log("Server runnin' on" + port);
