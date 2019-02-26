da611ed8b3480baaa2149d24c868669dd48ff9d9
Varnaan <derpbort@gmail.com>
2019-02-26 15:17:20 +0100

    Server added

---
+++ server.js
@@ -0,0 +1,22 @@
+const static = require('node-static');
+const http = require('http');
+
+let port = process.env.PORT;
+let directory = __dirname + '/public';
+
+if(typeof port == 'undefined' || !port){
+    directory = './public';
+    port = 8080;
+}
+
+let file = new.static.Server(directory);
+
+const app = http.createServer(
+    function(req, res) {
+        request.addListener('end' , 
+            function() {
+                file.server(req,res);
+            }
+        ).resume();
+    }
+).listen(port);
